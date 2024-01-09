06e58b6e54753e3f548454fe4596c4cf23741d90
SIDDHARTHJANGU <siddharthbishnoi1322@gmail.com>
2024-01-09 11:35:39 +0530

    all

--- src/Components/Myheader.jsx
+++ src/Components/Myheader.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+import nav_logo from "../assets/img/nav_logo.svg";
 
 const Myheader = () => {
   return (
     <div>
-      <div>
-        <p className=" text-black bg-red-300 text-9xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At, non
-          quaerat? Quasi distinctio quam provident et dolorum eos, reiciendis
-          aut?
-        </p>
-      </div>
+      <nav>
+        <div className=" container  max-w-[1144px] mx-auto px-3 ">
+          <div className=" flex justify-between items-center">
+            <div className=" flex items-center gap-[171px] ">
+              <img src={nav_logo} alt="img" />
+              <ul className=" flex items-center gap-[40px]">
+                <li>
+                  <a href="#Home"></a>Home
+                </li>
+                <li>
+                  <a href="#AboutUs"></a>About Us
+                </li>
+                <li>
+                  <a href="#Portfolio"></a>Portfolio
+                </li>
+                <li>
+                  <a href="#Services"></a>Services
+                </li>
+                <li>
+                  <a href="#ContactUs"></a>Contact Us
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button>Login</button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
